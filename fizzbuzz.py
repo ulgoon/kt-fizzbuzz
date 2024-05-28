d4632573037a0ba3920e2c70896845eeaa0799cc
ulgoon <ulgoon89@gmail.com>
2024-05-28 20:20:29 +0900

    refactor: Do fizzbuzz With 1 if
    Using %

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,5 @@
 for i in range(1,15+1):
-    if i % 15 == 0:
-        print('fizzbuzz', end=' ')
-    elif i % 3 == 0:
-        print('fizz', end=' ')
-    elif i % 5 == 0:
-        print('buzz', end=' ')
+    if i % 3 == 0 or i % 5 == 0:
+        print('fizz' * (i % 3 == 0) + 'buzz' * (i % 5 == 0), end=' ')
     else:
         print(i, end=' ')
